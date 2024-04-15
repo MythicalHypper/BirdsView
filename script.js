edda8a1ea29d129e084ece7b8ef5c882ecb5a2b1
MythicalHypper <gustavs.be@gmail.com>
2024-04-15 20:14:31 +0300

    Added more stuff

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
 $(document).ready(() => {
-    $("#headdin").hover(() => {$("#headdin").text("Enter")}, () => {$("#headdin").text("leave")})
+    $("#headdin").hover(function () { $(this).text("Enter") }, function () { $(this).text("leave") })
 
 
     $("#AKTUALITĀTES").hover(
-        () => {$(".fa-earth-americas").addClass("fa-bounce")},
-        () => {$(".fa-earth-americas").removeClass("fa-bounce")}
+        function () { $(".fa-earth-americas").addClass("fa-bounce") },
+        function () { $(".fa-earth-americas").removeClass("fa-bounce") }
     )
-    
+
     $("#CEĻOJUMI").hover(
-        () => {$(".fa-suitcase").addClass("fa-bounce")},
-        () => {$(".fa-suitcase").removeClass("fa-bounce")}
+        function () { $(".fa-suitcase").addClass("fa-bounce") },
+        function () { $(".fa-suitcase").removeClass("fa-bounce") }
     )
-    
+
     $("#PIETEIKŠANĀS-ANKETA").hover(
-        () => {$(".fa-square-pen").addClass("fa-bounce")},
-        () => {$(".fa-square-pen").removeClass("fa-bounce")}
+        function () { $(".fa-square-pen").addClass("fa-bounce") },
+        function () { $(".fa-square-pen").removeClass("fa-bounce") }
     )
-     
+
     $("#KONTAKTI").hover(
-        () => {$(".fa-user-group").addClass("fa-bounce")},
-        () => {$(".fa-user-group").removeClass("fa-bounce")}
+        function () { $(".fa-user-group").addClass("fa-bounce") },
+        function () { $(".fa-user-group").removeClass("fa-bounce") }
     )
 
     $("#PAR-MUMS").hover(
-        () => {$(".fa-user").addClass("fa-bounce")},
-        () => {$(".fa-user").removeClass("fa-bounce")}
+        function () { $(".fa-user").addClass("fa-bounce") },
+        function () { $(".fa-user").removeClass("fa-bounce") }
     )
 
 
+    var Ned_Italija_disp = true
+
+    function changeV(val) {
+        if (Ned_Italija_disp) {
+            $("p").attr("class", "")
+            $("p").addClass(`display-${val}`)
+        } else {
+            $("p").attr("class", "")
+            $("p").addClass(`h${val}`)
+        }
+    }
+
+    $("#Ned-Italija-btn").click(
+        function () {
+            Ned_Italija_disp = !Ned_Italija_disp
+            changeV($("#Ned-Italija-range").val())
+        }
+    )
+
+    $("#Ned-Italija-range").change(
+        function () { changeV(this.value) }
+    )
 })
